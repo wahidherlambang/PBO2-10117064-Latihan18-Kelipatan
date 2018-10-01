01b7d4a4a63f9b7015214b6309841cb8ed85d7b6
Wahid Herlambang Suroso <43497169+wahidherlambang@users.noreply.github.com>
2018-10-01 20:46:45 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117064/latihan18/kelipatan/PBO210117064Latihan18Kelipatan.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117064.latihan18.kelipatan;
+
+/**
+ *
+ * @author WAHID HERLAMBANG
+ * NAMA     : Wahid Herlambang Suroso
+ * KELAS    : PBO2
+ * NIM      : 10117064
+ * Deskripsi Program : Program ini berisi program untuk menampilkan hasil 
+ * dari kelipatan tertentu.
+ */
+public class PBO210117064Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i=3.5;
+        do{
+            if (i >= 3.5) {
+                System.out.println(i + "");
+            }
+             i +=3.5;
+        }
+        while (i <=35); {
+         
+    }
+        
+        
+        
+    }
+    
+}
